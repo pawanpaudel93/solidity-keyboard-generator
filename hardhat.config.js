d902271c778ea86d25b8cec188cad6e03276799c
Pawan Paudel <pawanpaudel93@gmail.com>
2022-06-22 22:37:16 +0545

    updated readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: process.env.NODE_API_URL,
+      url: process.env.RPC_URL,
       accounts: [process.env.RINKEBY_PRIVATE_KEY],
     },
   },
